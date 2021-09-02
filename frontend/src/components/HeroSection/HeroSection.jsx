8b1836ab2da709699f74c20e8233cfa51cd122a7
ThanhNhanDang <64689691+ThanhNhanDang@users.noreply.github.com>
2021-09-02 18:03:12 +0700

    first commit

---
+++ frontend/src/components/HeroSection/HeroSection.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function HeroSection() {
+    return (
+        <div className="hero-container">
+            <video src="/video/video-2.mp4" autoPlay loop muted></video>
+        </div>
+    )
+}
+
+export default HeroSection
